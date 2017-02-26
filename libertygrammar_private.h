e24ea4afd73c3faac883b9ff41424b034ac39300
Chrystian de Sousa Guth <csguth@gmail.com>
2017-02-26 14:53:04 +0000

    Enabled parsing of groups without name.   e.g., "timing() {}"
    Fixed problem parsing string values starting with digits like 1uW.

--- libertygrammar_private.h
+++ libertygrammar_private.h
@@ -63,9 +63,10 @@ namespace ast
 
     x3::real_parser<double, x3::strict_real_policies<double> > strictDouble;
     x3::rule<class name, std::string> name = "name";
+    x3::rule<class nonQuotedString, std::string> nonQuotedString = "nonQuotedString";
     x3::rule<class quotedString, std::string> quotedString = "quotedString";
     x3::rule<class stringValue, std::string> stringValue = "stringValue";
-    x3::rule<class value, boost::variant<double, int, std::string> > value = "value";
+    x3::rule<class value, SimpleAttribute::Value> value = "value";
     x3::rule<class simpleAttribute, SimpleAttribute> simpleAttribute = "simpleAttribute";
     x3::rule<class complexAttribute, ComplexAttribute> complexAttribute = "complexAttribute";
     x3::rule<class defineStatement, DefineStatement> defineStatement = "defineStatement";
@@ -75,17 +76,18 @@ namespace ast
 
     const auto name_def = (x3::char_("A-Za-z_") >> *(x3::char_("A-Za-z0-9_")));
     const auto quotedString_def = lexeme['"' >> *(char_ - '"') >> '"'];
-    const auto stringValue_def = (quotedString | name);
-    const auto value_def = (strictDouble | int_ | stringValue);
+    const auto nonQuotedString_def = *x3::char_("0-9") >> x3::char_("A-Za-z_") >> *x3::char_("A-Za-z-0-9_");
+    const auto stringValue_def = (quotedString | nonQuotedString);
+    const auto value_def = (strictDouble | stringValue | int_);
     const auto simpleAttribute_def = name >> ':' >> value >> ';';
     const auto complexAttribute_def = name >> '(' >> (value % ',')  >> ')' >> lit(";");
     const auto defineStatement_def = lit("define") >> '(' >> name >> ',' >> name >> ',' >> attribute >> ')' >> lit(";");
     const auto attributeList_def = ('{' >> *attributeStatement >> '}');
-    const auto groupStatement_def = (name >> ('('>> stringValue >> ')') >> attributeList);
+    const auto groupStatement_def = (name >> ('('>> -stringValue >> ')') >> attributeList);
     const auto attributeStatement_def = (defineStatement|simpleAttribute|complexAttribute|groupStatement);
     const auto spaceComment = x3::rule<class spaceComment>{} = ascii::space | lexeme[lit("/*") >> *(char_ - x3::string("*/")) >> lit("*/")];
 
-    BOOST_SPIRIT_DEFINE(name, quotedString, stringValue, value, simpleAttribute, complexAttribute, defineStatement, attributeList, attributeStatement, groupStatement)
+    BOOST_SPIRIT_DEFINE(name, quotedString, nonQuotedString, stringValue, value, simpleAttribute, complexAttribute, defineStatement, attributeList, attributeStatement, groupStatement)
 
 }
 }
